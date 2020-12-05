155b6528106cfc753a134f8174caa6ee670ac555
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-05 11:04:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function mapToNegativize(arr){
+  let newArray = []
+  for (let i = 0; i < arr.length; i++) {
+    newArray[i] = arr[i] * -1
+  }
+  return newArray
+}
+
+function mapToNoChange(arr){
+  let newArray=[]
+  for(let i=0;i<arr.length;i++){
+  newArray[i]=arr[i]}
+  return newArray
+}
